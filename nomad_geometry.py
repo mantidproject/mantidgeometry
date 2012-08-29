5d886bb836f48aeec07f3763737d1f461416f082
Peter Peterson <petersonpf@ornl.gov>
2012-08-29 11:07:49 -0400

    Adding in Joerg's idl source code.
    Since we should have a direct port of it, just put it in as multi-line comments.

--- nomad_geometry.py
+++ nomad_geometry.py
@@ -76,6 +76,37 @@ if __name__ == "__main__":
     #info = instr.addDetectorIds("Group6", makeIds(18, 82944, 8*128))
 
     # ---------- add in group1
+"""
+;;;source idl code
+N_first=14
+z0_first=6.41/8.45*3.2
+x0_first=1.31/8.45*3.2
+z1_first=3.86/8.45*3.2
+x1_first=1.44/8.45*3.2
+dx_first=x1_first-x0_first
+dz_first=z1_first-z0_first
+section=360/float(n_first)
+
+onehundredtwentyeight=findgen(128) ; 0->127
+
+for i=0,n_first-1 do begin
+angle=(360*(i+.5)/float(N_first))*!dtor
+x0=-x0_first*cos(section*i*!dtor)
+y0=x0_first*sin(section*i*!dtor)
+x1=-x1_first*cos(section*i*!dtor)
+y1=x1_first*sin(section*i*!dtor)
+
+for j=0,7 do begin
+x0j=x0+j*(.0254+0.001)*sin(section*(i+.5)*!dtor)
+y0j=y0+j*(.0254+0.001)*cos(section*(i+.5)*!dtor)
+x1j=x1+j*(.0254+0.001)*sin(section*(i+.5)*!dtor)
+y1j=y1+j*(.0254+0.001)*cos(section*(i+.5)*!dtor)
+x(i*8+j,*)=x0j+(x1j-x0j)*(1-onehundredtwentyeight/128.)
+y(i*8+j,*)=y0j+(y1j-y0j)*(1-onehundredtwentyeight/128.)
+z(i*8+j,*)=z0_first+dz_first*(1-onehundredtwentyeight/128.)
+end
+end
+"""
     group1 = instr.makeTypeElement("Group1")
     z = .5*((6.41/8.45*3.2) + (3.86/8.45*3.2))# 1.940845
     det = instr.makeDetectorElement("one_inch", root=group1)
@@ -136,6 +167,41 @@ if __name__ == "__main__":
             rot_inner=92.774014947, rot_innermost=77.1274554878)
 
     # ---------- add in group2
+"""
+;;; source idl code
+N_second=23
+;z0_second=5.05/8.45*3.2
+;x0_second=2.24/8.45*3.2
+;z1_second=2.54/8.45*3.2
+;x1_second=2.24/8.45*3.2
+z0_second=5.09/7.06*2.7-0.0095
+x0_second=2.22/7.06*2.7
+z1_second=2.45/7.06*2.7+0.0095
+x1_second=2.22/7.06*2.7
+dx_second=x1_second-x0_second
+dz_second=z1_second-z0_second
+section=360/float(n_second)
+
+
+
+for i=0,n_second-1 do begin
+angle=(360*(i+.5)/float(N_second))*!dtor
+x0=-x0_second*cos(section*i*!dtor)
+y0=x0_second*sin(section*i*!dtor)
+x1=-x1_second*cos(section*i*!dtor)
+y1=x1_second*sin(section*i*!dtor)
+
+for j=0,7 do begin
+x0j=x0+j*(.0254+0.001)*sin(section*(i+.5)*!dtor)
+y0j=y0+j*(.0254+0.001)*cos(section*(i+.5)*!dtor)
+x1j=x1+j*(.0254+0.001)*sin(section*(i+.5)*!dtor)
+y1j=y1+j*(.0254+0.001)*cos(section*(i+.5)*!dtor)
+x((i+n_first)*8+j,*)=x0j+(x1j-x0j)*onehundredtwentyeight/128.
+y((i+n_first)*8+j,*)=y0j+(y1j-y0j)*onehundredtwentyeight/128.
+z((i+n_first)*8+j,*)=z0_second+dz_second*onehundredtwentyeight/128.
+end
+end
+"""
     group2 = instr.makeTypeElement("Group2")
     z = .5*((5.09/7.06*2.7-0.0095) + (2.45/7.06*2.7+0.0095)) # 1.445654
     det = instr.makeDetectorElement("one_inch", root=group2)
@@ -232,6 +298,44 @@ if __name__ == "__main__":
             rot_inner=90.0, rot_innermost=82.173807641)
 
     # ---------- add in group3
+"""
+;;; source idl code
+N_third=14
+ii=[3,4,5,6,7,8,9,18,19,20,21,22,23,24]+.5
+;z0_third=2.60/8.45*3.2
+;x0_third=2.56/8.45*3.2
+;z1_third=0/8.45*3.2
+;x1_third=2.56/8.45*3.2
+z0_third=2.59/7.06*2.7-0.0076
+;x0_third=2.61/7.06*2.7
+x0_third=1.00
+z1_third=-0.04/7.06*2.7+.0076
+;x1_third=2.61/7.06*2.7
+x1_third=1.00
+dx_third=x1_third-x0_third
+dz_third=z1_third-z0_third
+section=360/float(29)
+
+
+
+for i=0,n_third-1 do begin
+angle=(360*(ii(i)+.5)/float(N_third))*!dtor
+x0=-x0_third*cos(section*ii(i)*!dtor)
+y0=x0_third*sin(section*ii(i)*!dtor)
+x1=-x1_third*cos(section*ii(i)*!dtor)
+y1=x1_third*sin(section*ii(i)*!dtor)
+
+for j=0,7 do begin
+x0j=x0+j*(.0254+0.001)*sin(section*(ii(i)+.5)*!dtor)
+y0j=y0+j*(.0254+0.001)*cos(section*(ii(i)+.5)*!dtor)
+x1j=x1+j*(.0254+0.001)*sin(section*(ii(i)+.5)*!dtor)
+y1j=y1+j*(.0254+0.001)*cos(section*(ii(i)+.5)*!dtor)
+x((i+n_first+n_second)*8+j,*)=x0j+(x1j-x0j)*(1-onehundredtwentyeight/128.)
+y((i+n_first+n_second)*8+j,*)=y0j+(y1j-y0j)*(1-onehundredtwentyeight/128.)
+z((i+n_first+n_second)*8+j,*)=z0_third+dz_third*(1-onehundredtwentyeight/128.)
+end
+end
+"""
     group3 = instr.makeTypeElement("Group3")
     z = .5*((2.59/7.06*2.7-0.0076) + (-0.04/7.06*2.7+.0076)) # 0.48373677
     det = instr.makeDetectorElement("one_inch", root=group3)
@@ -292,7 +396,8 @@ if __name__ == "__main__":
             rot_inner=90.0, rot_innermost=40.3447402631)
 
     # ---------- add in group4
-    """
+"""
+;;; source idl code
 N_forth=12
 ii=[2,3,4,5,6,7,13,14,15,16,17,18]+1
 ;z0_forth=-.2/8.45*3.2
@@ -386,6 +491,21 @@ end
         rect.makeLocation(instr, det, bank)
 
     # ---------- add in group5
+"""
+;;; source idl code
+N_back=19
+z0_back=-1.78/8.45*3.2
+y0_back=1.32/8.45*3.2
+for i=0,n_back-1 do begin
+
+for j=0,7 do begin
+ntube=i*8+j
+x((i+n_first+n_second+n_third+n_fourth)*8+j,*)=(1-onehundredtwentyeight/128.)-0.5
+y((i+n_first+n_second+n_third+n_fourth)*8+j,*)=y0_back-(.0254/2+.001)/2*ntube
+z((i+n_first+n_second+n_third+n_fourth)*8+j,*)=z0_back-((i/2)*2 eq i)*(.0254/2+.001)
+end
+end
+"""
     group5 = instr.makeTypeElement("Group5")
     y = 0. # 0.003906
     z = -1.78/8.45*3.2 # -0.687783
@@ -444,7 +564,24 @@ end
     makeLoc(instr, det, "bank81",
             x=0.483093, y=y, z=z, rot=180.0)
 
-    # ---------- add in group6
+    # ---------- add in group6 - results match
+"""
+;;; source idl code
+
+N_forward=19
+z0_forward=6.69/8.45*3.2
+y0_forward=1.34/8.45*3.2
+for i=0,n_forward-1 do begin
+
+for j=0,7 do begin
+ntube=i*8+j
+x((i+n_first+n_second+n_third+n_fourth+n_back)*8+j,*)=(1-onehundredtwentyeight/128.)*.9-0.9/2
+;;; hack: shift the forward panel by some
+y((i+n_first+n_second+n_third+n_fourth+n_back)*8+j,*)=y0_forward-(.0254/2+.001)/2*(ntube+32)+.062
+z((i+n_first+n_second+n_third+n_fourth+n_back)*8+j,*)=z0_forward-((j/2)*2 eq j)*(.0254/2+.001)
+end
+end
+"""
     group6 = instr.makeTypeElement("Group6")
     y = 0.0078125 # 0. # 0.003906
     z = 6.69/8.45*3.2# 2.51979
