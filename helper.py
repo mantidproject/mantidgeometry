6cff0de1e030a74bfababd9142a4f87b3e4a81bb
Peter Peterson <petersonpf@ornl.gov>
2013-10-24 14:57:30 -0400

    Observing the 'optional' neutronic information.

--- helper.py
+++ helper.py
@@ -156,7 +156,8 @@ class MantidGeom:
                 zi=float(distance[i]) # check if float
                 zi=str(zi) # convert it to a string for lxml
                 location = le.SubElement(basecomponent, "location", z=zi, name=names[i])
-                le.SubElement(location, "neutronic", z=zi)
+                if neutronic:
+                    le.SubElement(location, "neutronic", z=zi)
             except:
                 pos_loc=le.SubElement(basecomponent, "location",name=names[i])
                 processed=split(str(distance[i]))
