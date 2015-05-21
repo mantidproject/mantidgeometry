bf6b8034b76079dfed129993e6e21ef74772b8b6
Wenduo Zhou <zhouw@ornl.gov>
2015-05-21 15:53:33 -0400

    Refs #11190. Updated HB3A geometry generation file.

--- hb3a_geometry.py
+++ hb3a_geometry.py
@@ -15,7 +15,7 @@ import helper as helper
 HB3AParam = {
     "source_distance": 2.0,
     "sample_position": [0., 0., 0.],
-    "detector_distance": 1.0,
+    "detector_distance": 0.3518,
     "detector_size": [2.*0.0254, 2.*0.0254], # 2 inch x 2 inch
     "pixel_number": [256, 256],
     }
@@ -30,7 +30,23 @@ def main(argv):
         outidfname = INST_NAME+"_Definition.xml"
     else: 
         outidfname = argv[1]
-    
+
+
+    # Calculate some dimension parameters for HB3A
+    detsize_x = HB3AParam["detector_size"][0]
+    numpixel_x = HB3AParam["pixel_number"][0]
+    pixelsize_x = detsize_x/numpixel_x
+
+    detsize_y = HB3AParam["detector_size"][1]
+    numpixel_y = HB3AParam["pixel_number"][1]
+    pixelsize_y = detsize_y/numpixel_y
+
+    xrightpixel = (numpixel_x-1)*0.5*pixelsize_x
+    ytoppixel = (numpixel_y-1)*0.5*pixelsize_y
+
+    #print "Pixel size : %.6f X %.6f" % (pixelsize_x, pixelsize_y)
+    #print "Right-most pixel position = %.6f, Top pixel position = %.6f" % (xrightpixel, ytoppixel)
+
     # initialize MantidGeom object
     instname = INST_NAME    
     comment = "Created by Wenduo Zhou"
@@ -50,7 +66,10 @@ def main(argv):
 
     # add panel
     hb3a.addComment("PANEL")
-    panel = hb3a.addComponent2(type_name='panel', idstart=1, idfillbyfirst='y', idstepbyrow=256, blank_location=False)
+    if False: 
+        panel = hb3a.addComponent2(type_name='panel', idstart=1, idfillbyfirst='y', idstepbyrow=256, blank_location=False)
+    else: 
+        panel = hb3a.addComponent2(type_name='panel', idstart=1, idfillbyfirst='x', idstepbyrow=256, blank_location=False)
     hb3a.addLocationRTP2(root=panel, name='bank1', 
             r = HB3AParam["detector_distance"], 
             t = '2theta -1.0*2theta+0.0', 
@@ -59,28 +78,50 @@ def main(argv):
             rot_y = '2theta -1.0*2theta+0.0',
             rot_z = None)
 
-    xpixels="256" 
-    xstart="-0.078795" 
-    xstep="+0.000618"
-    ypixels="256" 
-    ystart="-0.078795" 
-    ystep="+0.000618"
+    #if False:
+    #    xpixels="256" 
+    #    xstart="-0.078795" 
+    #    xstep="+0.000618"
+    #    ypixels="256" 
+    #    ystart="-0.078795" 
+    #    ystep="+0.000618"
+    #else:
+    #    xpixels="256" 
+    #    xstart="0.078795" 
+    #    xstep="-0.000618"
+    #    ypixels="256" 
+    #    ystart="-0.078795" 
+    #    ystep="+0.000618"
+
+    # Define pixel positions in a rectangular detector and add detector
+    xpixels = str(numpixel_x)
+    xstart  = str(xrightpixel)
+    xstep   = str(-1.*pixelsize_x)
+
+    ypixels = str(numpixel_y)
+    ystart  = str(ytoppixel)
+    ystep   = str(-1.*pixelsize_y)
+
     hb3a.addRectangularDetector(None, xpixels, xstart, xstep, ypixels, ystart, ystep)
 
-    # add pixel information
+    # Add pixel information
+    lfb_pt = [-1.*pixelsize_x*0.5, -1.*pixelsize_y*0.5, 0.0]      # left-front-bottom
+    lft_pt = [-1.*pixelsize_x*0.5,     pixelsize_y*0.5, 0.0]      # left-front-top
+    lbb_pt = [-1.*pixelsize_x*0.5, -1.*pixelsize_y*0.5, -0.0001]  # left-back-bottom
+    rfb_pt = [    pixelsize_x*0.5, -1.*pixelsize_y*0.5, 0.0]      # right-front-bottom
+
     hb3a.addCuboidPixel(name="pixel", 
-                        lfb_pt = [-0.000309, -0.000309, 0.0],
-                        lft_pt = [-0.000309,  0.000309, 0.0],
-                        lbb_pt = [-0.000309, -0.000309, -0.0001],
-                        rfb_pt = [ 0.000309, -0.000309, 0.0],
-                        is_type="detector", shape_id="pixel-shape")
+                        lfb_pt = lfb_pt,
+                        lft_pt = lft_pt,
+                        lbb_pt = lbb_pt,
+                        rfb_pt = rfb_pt,
+                        is_type="detector", 
+                        shape_id="pixel-shape")
 
-
-    hb3a.showGeom()
 
     # write geometry
-    hb2a.showGeom()
-    hb2a.writeGeom(outidfname)
+    hb3a.showGeom()
+    hb3a.writeGeom(outidfname)
     
     return    
     
