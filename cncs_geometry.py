5cfac68fcc3f72f7df1d36ff1869d7af7e759326
Andrei Savici <saviciat@ornl.gov>
2015-08-17 14:51:28 -0400

    Fix line endings for CNCS

--- cncs_geometry.py
+++ cncs_geometry.py
@@ -27,12 +27,12 @@ if __name__ == "__main__":
     try:
         geom_input_file = sys.argv[1]
     except IndexError:
-        geom_input_file = "SNS/CNCS/CNCS_geom_110136-.txt"
+        geom_input_file = "SNS/CNCS/CNCS_geom_136675-.txt"
         
     # Set header information
     comment = "Created by Michael Reuter"
     # Time needs to be in UTC?
-    valid_from = "2015-01-30 00:00:00"
+    valid_from = "2015-08-01 00:00:00"
 
     # Get geometry information file
     inst_name = "CNCS"
