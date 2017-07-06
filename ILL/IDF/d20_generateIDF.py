ef58fe52b14b34b6c34534570510d51a62b1ef56
Gagik Vardanyan <vardanyan@ill.fr>
2017-07-06 16:07:40 +0200

    Re #0 2theta sign D20, D4

--- ILL/IDF/d20_generateIDF.py
+++ ILL/IDF/d20_generateIDF.py
@@ -56,6 +56,7 @@ def printHeader():
         <along-beam axis="z" />
         <pointing-up axis="y" />
         <handedness val="right" />
+        <theta-sign axis="x" />
       </reference-frame>
     </defaults>
     """
