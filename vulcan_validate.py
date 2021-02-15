0037be78b47ee835776a60e5ce14f51b4fcacfb2
Pete Peterson <petersonpf@ornl.gov>
2021-02-15 17:08:49 -0500

    First pass at verifying the geometry is correct

---
+++ vulcan_validate.py
@@ -0,0 +1,63 @@
+from mantid.simpleapi import LoadEmptyInstrument
+import numpy as np
+
+
+def getTubeIds(compInfo, name):
+    # the component index for the bank as a whole
+    bankID = int(compInfo.indexOfAny(name))
+
+    # the bank is made of 8-packs
+    bank = []
+    eightpacks = compInfo.children(bankID)
+    for eightpack in eightpacks:
+        tubes = compInfo.children(int(eightpack))
+        eightpack = [compInfo.children(int(tube)) for tube in tubes]
+        bank.extend(eightpack)
+
+    # convert the result into a 2d array of integer indices
+    return np.asarray(bank, dtype=int)
+
+
+def getPositions(ids, compInfo):
+    x = np.zeros(ids.shape, dtype=float)
+    y = np.zeros(ids.shape, dtype=float)
+    z = np.zeros(ids.shape, dtype=float)
+    for i in range(ids.shape[0]):   # tube
+        for j in range(ids.shape[1]):  # pixel along the tube
+            x[i, j], y[i, j], z[i, j] = compInfo.position(int(ids[i, j]))
+    return x, y, z
+
+
+vulcan = LoadEmptyInstrument(Filename='VULCAN_Definition.xml', OutputWorkspace='vulcan')
+compInfo = vulcan.componentInfo()
+# detInfo = vulcan.detectorInfo()
+
+
+for name in ['bank1', 'bank2', 'bank5']:
+    print('=========================', name)
+    bank = getTubeIds(compInfo, name)
+    x, y, z = getPositions(bank, compInfo)
+
+    # confirm that the positions are constant in certain directions
+    np.testing.assert_equal(x[:, 1:] - x[:, :-1], 0., err_msg="everything in same tube has same x")
+    np.testing.assert_equal(y[1:, :] - y[:-1, :], 0., err_msg="everything in same row has same y")
+    np.testing.assert_equal(z[:, 1:] - z[:, :-1], 0., err_msg="everything in same tube has same z")
+
+    # confirm that lower-left is in the correct place
+    # positions that are checked to 0.1mm
+    assert y[0, 0] == y.min(), 'y-value'  # always gravitationally down
+    if name == 'bank1':
+        np.testing.assert_almost_equal(x[0, 0], x.max(), err_msg='x-value lower-left', decimal=4)
+        np.testing.assert_almost_equal(z[0, 0], z.min(), err_msg='x-value lower-left', decimal=4)
+    elif name == 'bank2':
+        np.testing.assert_almost_equal(x[0, 0], x.min(), err_msg='x-value lower-left', decimal=4)
+        np.testing.assert_almost_equal(z[0, 0], z.max(), err_msg='x-value lower-left', decimal=4)
+        # bank2 x-max, z-max
+    else:
+        # bank5 x in front plane, z-max, but looks complicated
+        print(f'NOT checking location of lower-left for {name}')
+
+    # confirm that the positions are increasing in other directions
+    np.testing.assert_array_less(y[:, :-1], y[:, 1:], err_msg="everything in same row has increasing y")
+    # TODO check X
+    # TODO check Z
