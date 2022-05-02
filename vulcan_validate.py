6751494c6ddb2afe63bd8e74a531f17c9510178d
Chen Zhang <zhangc@ornl.gov>
2022-05-02 12:58:11 -0400

    update hybrid csv and idf validator

--- vulcan_validate.py
+++ vulcan_validate.py
@@ -1,6 +1,13 @@
-from mantid.simpleapi import LoadEmptyInstrument
+#!/usr/bin/env python3
+
+"""
+Validate default output VULCAN IDF file from vulcan_geometry.py
+"""
+
+import logging
 import numpy as np
-from vulcan_geometry import readPositions
+from vulcan_geometry import read_survey_measurements
+from mantid.simpleapi import LoadEmptyInstrument
 
 
 def getTubeIds(compInfo, name):
@@ -24,70 +31,118 @@ def getPositions(ids, compInfo):
     x = np.zeros(ids.shape, dtype=float)
     y = np.zeros(ids.shape, dtype=float)
     z = np.zeros(ids.shape, dtype=float)
-    for i in range(ids.shape[0]):   # tube
+    for i in range(ids.shape[0]):  # tube
         for j in range(ids.shape[1]):  # pixel along the tube
             x[i, j], y[i, j], z[i, j] = compInfo.position(int(ids[i, j]))
     return x, y, z
 
 
 def position_to_str(x, y, z):
-    return f'{x:7.4f}, {y:7.4f}, {z:7.4f}'
+    return f"{x:7.4f}, {y:7.4f}, {z:7.4f}"
 
 
 if __name__ == "__main__":
-    vulcan = LoadEmptyInstrument(Filename='VULCAN_Definition_tmp.xml', OutputWorkspace='vulcan')
+    logging.basicConfig(
+        format="[%(levelname)s]%(asctime)s:%(message)s", level=logging.INFO
+    )
+    #
+    lb_bank = 'bank'
+    lb_8pack = 'eightpack'
+    lb_pos = ['X','Y','Z']
+    df_survey = read_survey_measurements()
+    logging.info("Load sanitized survey measurements.")
+    logging.info(f"\n{df_survey.groupby(lb_bank)[lb_pos].mean()}")
+    for bank_id in df_survey[lb_bank].unique():
+        logging.info(f"bank_{bank_id}\n{df_survey[df_survey[lb_bank]==bank_id][lb_pos]}")
+    #
+    logging.info("Loading VULCAN instrument definition.")
+    vulcan = LoadEmptyInstrument(
+        Filename="VULCAN_Definition_tmp.xml", OutputWorkspace="vulcan"
+    )
     compInfo = vulcan.componentInfo()
     detInfo = vulcan.detectorInfo()
 
-    banks_exp = readPositions()
-    for name in ['bank1', 'bank2', 'bank3', 'bank4', 'bank5']:#, 'bank6']:
-        print('--------------', name)
-        for point in banks_exp[name].points:
-            print(point)
-
-    for name in ['bank1', 'bank2', 'bank3', 'bank4', 'bank5']:#, 'bank6']:
-        print('=========================', name)
+    # -- Verify banks are in desired quadrants
+    #
+    #              bank5 | bank4
+    #                    |      bank 3
+    # bank1------------sample---------->(x) bank2
+    #              bank6 |
+    #                    v     incident beam
+    #                   (z)
+    logging.info("Verify banks are in desired quadrants.")
+    for name in ['bank1', 'bank2', 'bank3', 'bank4', 'bank5', 'bank6']:
+        logging.info(f"Checking {name}")
         bank = getTubeIds(compInfo, name)
         x, y, z = getPositions(bank, compInfo)
-
-        # confirm which quadrants things are in - x-axis
-        #                  bank5 | bank4
-        #                        |      bank 3
-        # bank1----------------sample---------->(x) bank2
-        #                  bank6 |
-        #                        v     incident beam
-        #                       (z)
-        if name in ['bank1','bank5']:
+        if name in ['bank1','bank5', 'bank6']:
             assert np.alltrue(x < 0.)
         elif name in ['bank4', 'bank3','bank2']:
             assert np.alltrue(x > 0.)
-            # confirm which quadrants things are in - z-axis
+        # confirm which quadrants things are in - z-axis
         if name in ['bank3','bank4','bank5']:
             assert np.alltrue(z < 0.)
+        if name in ["bank6"]:
+            assert np.alltrue(z > 0.)
 
-        # confirm that the y-center bank center
-        center_exp = banks_exp[name].center
-        np.testing.assert_almost_equal(x.mean(), center_exp.x, err_msg='detector panel is x-center',
-                                       decimal=4)
-        np.testing.assert_almost_equal(y.mean(), center_exp.y, err_msg='detector panel is y-center',
-                                       decimal=4)
-        np.testing.assert_almost_equal(z.mean(), center_exp.z, err_msg='detector panel is z-center',
-                                       decimal=4)
+    # -- Verify center positions of 8-packs in each bank
+    # bank_1: 20
+    # bank_2: 20
+    # bank_3: 18
+    # bank_4: 18
+    # bank_5: 9
+    # bank_6: 11
+    logging.info("Verify center positions of 8-packs in each bank.")
+    instr = vulcan.getInstrument()
+    offset = 2  # bank1 is the 4th item in the list
+    for bank_id in df_survey[lb_bank].unique():
+        # from survey measurements
+        bank_survey = df_survey[df_survey[lb_bank]==bank_id]
+        centers_survey = bank_survey.groupby(lb_8pack).mean()[lb_pos]
+        logging.info(f"Bank {bank_id}'s 8-pack center positions from survey:\n{centers_survey}")
+        # from IDF
+        bank_id_mantid = int(bank_id) + offset
+        bank_mantid = instr[bank_id_mantid]
+        centers_mantid = np.array([bank_mantid[pack_id].getPos() for pack_id in range(bank_mantid.nelements())])
+        logging.info(f"Bank {bank_id}'s 8-pack center positions from IDF:\n{centers_mantid}")
+        # verify
+        np.testing.assert_array_almost_equal(
+            centers_survey.to_numpy(),
+            centers_mantid,
+            err_msg=f"Bank {bank_id}'s 8-pack center positions do not match.",
+            decimal=4,
+        )
 
-        # confirm that the positions are constant in certain directions
-        np.testing.assert_almost_equal(x[:, 1:] - x[:, :-1], 0., decimal=4,
-                                       err_msg="everything in same tube has same x")
-        np.testing.assert_almost_equal(y[1:, :] - y[:-1, :], 0., decimal=4,
-                                       err_msg="everything in same row has same y")
-        np.testing.assert_almost_equal(z[:, 1:] - z[:, :-1], 0., decimal=4,
-                                       err_msg="everything in same tube has same z")
+    # -- Verify the interleaving tubes
+    logging.info("Verify the interleaving tubes.")
+    instr = vulcan.getInstrument()
+    offset = 2  # bank1 is the 4th item in the list
+    for bank_id in df_survey[lb_bank].unique():
+        bank_id_mantid = int(bank_id) + offset
+        bank_mantid = instr[bank_id_mantid]
+        for pack_id in range(bank_mantid.nelements()):
+            pack = bank_mantid[pack_id]
+            pack_name = pack.getName()
+            tube_L2s = []
+            for tube_id in range(pack.nelements()):
+                tube = pack[tube_id]
+                tube_name = tube.getName()
+                tube_L2 = np.linalg.norm(tube.getPos())
+                logging.info(f"Bank{bank_id}'s 8-pack {pack_name}'s tube {tube_name}'s L2: {tube_L2}")
+                tube_L2s.append(tube_L2)
+            tube_L2s = np.array(tube_L2s)
+            # verify
+            back_minus_front_l2 = tube_L2s[:-1:2] - tube_L2s[1::2]
+            if not np.alltrue(back_minus_front_l2 > 0.0):
+                logging.error(f"back: {tube_L2s[:-1:2]}")
+                logging.error(f"front: {tube_L2s[1::2]}")
+                logging.error(f"back-front: {back_minus_front_l2}")
+            assert np.alltrue(back_minus_front_l2 > 0.0)
 
-        # verify the interleaving tubes
-        distances = np.sqrt(np.square(x[:, 256]) + np.square(z[:, 256]))  # distance of in-plane
-        delta = distances[1:] - distances[:-1]  # every other tube is same distance
-        if name in ['bank1', 'bank2', 'bank3', 'bank4', 'bank5']:
-            assert np.alltrue(delta[::2] < 0.)
-            assert np.alltrue(delta[1::2] > 0.)
-
-        # confirm that the positions are increasing in other directions
+    # -- Verify the positions  are increasing in other directions
+    logging.info("Verify the positions are increasing in other directions.")
+    for name in ['bank1', 'bank2', 'bank3', 'bank4', 'bank5', 'bank6']:
+        logging.info(f"Checking {name}")
+        bank = getTubeIds(compInfo, name)
+        x, y, z = getPositions(bank, compInfo)
         np.testing.assert_array_less(y[:, :-1], y[:, 1:], err_msg="everything in same row has increasing y")
